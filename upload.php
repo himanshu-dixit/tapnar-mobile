11edf849a113624aaa6baad9da760b9cd48e9d16
Himanshu Dixit <hudixt@gmail.com>
2016-10-23 17:02:42 +0530

    Intialized Upload

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title>Upload | Tapnar</title>
+  <title>Tapnar | Best Images on Internet Worth Clicking</title>
   <meta charset="utf-8" />
 <meta name="description" content="Tapnar | Most Popular Images & GIF On Internet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,139 +9,81 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="assests/upload.css" />
-  <link rel="stylesheet" href="assests/common.css" />
+ <script src="parts/jquery.jscroll.js"></script>
 
-<link rel="stylesheet" href="assests/dropzone.css">
+
+ <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="assests/index.css" />
+    <link rel="stylesheet" href="assests/upload.css" />
 </head>
 <body>
+  <!-- Swipe Menu Start -->
+<?php include 'parts/menu.php';?>
+
+  <!-- Swipe Menu Ends -->
 <?php include 'parts/header.php';?>
-</div>
-</div>
-<!-- Top Bar Ends -->
-<!-- COntent Starts -->
-<?php include 'parts/category_box.php'; ?>
-
-<div class="content" id="preview" >
-  <div class="container main_image_body">
 
 
-<div id ="upload_bar">
-<div class="upload_input_url">
-
-    <input id="enter_url" name="enter_url" placeholder="Enter Image or Video Url To import" type="text" style="padding-left:16px;" class="url">
-
-
-</div>
-<div class="browse_button" id="url_submit">
-
-<center>Enter<center>
-</div>
-</div>
+  <video poster="http://tapnar.com/assests/login.jpg" id="bgvid" playsinline autoplay muted loop>
+      <source src="https://media.giphy.com/media/JOK5PP5Ol2Hzq/giphy.mp4" type="video/mp4">
+  </video>
 
 
 
+<div class="content" id="start">
 
-<div id="drop_area" class="drag_drop_area"><form enctype= "multipart/form-data" type="file" name="file" class="needsclick dz-message"  id="drag-drop">
+<br><br>
+<div class="container main_image_body" >
+  <center><div class="logo"><img src="http://tapnar.com/resources/3s.png" height="54px" style="margin: -1px 0px 0px -24px" /></div></center><br>
 
-  <div class="needsclick" id="update">
 
-<center>Click To Upload / DRAG & DROP HERE</center>
+<div>
+<center>
+<div class="facebook_login" id="facebook_connect"><center>Login With Facebook</center></div></center>
+
+<div class="or">
+  <center>OR</center></div>
+
+
+<center>
+  <div class="email">
+    <input id="email" name="email" placeholder="Email" type="text" style="width:400px;height:60px;padding-left:15px; border: none !important;">
   </div>
-
-</form></div>
-
-<div id="processing" class="drag_drop_area">
-<center>
-
-<br>
-<img src="assests/loading.gif" height="100px" style="margin-top:20px;" />
 </center>
-<center>
-<div class="value_process">
 
+
+<center>
+  <div class="password">
+    <input id="password" name="password" placeholder="Enter Your  Password" type="password" style="width:400px;height:60px;padding-left:15px;margin-top:10px; border: none !important;">
+  </div>
+</center>
+
+<center>
+<div class="button" id="email_connect"><center>LOGIN</center></div></center>
+
+<!--<center>
+<div class="button" id="email_connect" style="background-color:#fdb94f;"><center>SINGUP</center></div></center>
+-->
+
+<div class="join_tapnar" style="margin-top:20px;"><center>
+<span class="no_account">Don't Have A Account | </span> <a href="signup.php"><span class="join_tapnar">SIGN UP</span></a>
+</center>
 </div>
+
+
+<div class="forgot_password" style="margin-top:10px;"><center>
+<a href="login.php"><span class="forgot_password">Forgot Password </span></a>
+
+
 </center>
 </div>
 
 </div>
-</div>
-</div>
-
-
-
-<div class="content" id="process_area" >
-  <div class="container main_image_body">
-
-</div>
-</div>
-  <!-- footer Starts -->
-<?php include('parts/footer'); ?>
-  <script src="/parts/js/dropzone.js" ></script>
 <script>
 
-Dropzone.autoDiscover = false;
-var instance = null;
-$("#drop_area").dropzone({
-    init: function() {
-        var $this = this;
-        instance = this;
-      console.log("hell");
-    },
-        url : 'module/uploaded',
-    paramName: "file",
-    maxFilesize: 1000,
-    maxFiles : 1,
-    autoProcessQueue : true,
-    acceptedFiles : 'image/*,.mp4'
-
-});
-
-$(function() {
-  // Now that the DOM is fully loaded, create the dropzone, and setup the
-  // event listeners
-
-instance.on("success", function(file, response) {
-  var go_to_id = response;
-window.location.assign(response);
-  });
-})
-
-$(function() {
-  // Now that the DOM is fully loaded, create the dropzone, and setup the
-  // event listeners
-
-instance.on("uploadprogress", function(file, progress) {
-document.getElementById('').innerHTML = "<center>"+progress+" % Uploaded </center>"
-      console.log(progress);
-  });
-})
-
-</script>
-
-
-<script>
-$('#url_submit').click(function(){
- var enter_url = document.getElementById("enter_url").value;
- if(enter_url){
-           document.getElementById('drop_area').setAttribute("style","display:none");
- document.getElementById('processing').setAttribute("style","display:block");
- var post_url = document.getElementById("enter_url").value;
- $.post("module/remote_url",
-   {
-       url: post_url
-   },
-   function(data, status){
-    //   alert("Data: " + data + "\nStatus: " + status);
-       window.location.assign(data);
-   });
-
-}
-else{
-  alert('Please enter Url');
-}
-});
-
-</script>
-</body>
+  </script>
+</div>
 </html>
